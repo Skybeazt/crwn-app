c0de9119c25947caac5f0524e71f12ff0002334e
skybeazt <arikupaul9@gmail.com>
2022-01-27 07:36:16 +0000

    Broken our homepage into smaller components, finished styling our menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./homepage.component";
+// import HomePage from "./pages/homePage/homepage.component";
+import HomePage from "./pages/homepage/hompage.component";
 
 function App() {
   return (
